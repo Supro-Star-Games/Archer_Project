fd8635e6d37c05915cbf0a59ca2e4cad0f734e45
supromental <130070014+supromental@users.noreply.github.com>
2023-09-15 22:04:34 +0300

    #Добавить рендер траектории - Выполнено

--- Assets/Archer_Project/Scripts/Archer.cs
+++ Assets/Archer_Project/Scripts/Archer.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,36 +10,48 @@ public class Archer : MonoBehaviour
 	[SerializeField] private float _fireAngle;
 	private Vector3 _direction;
 	private Vector3 _directionXZ;
+	private float _projectileVelocity;
 
+	public Vector3 ProjectileVelocity { get; set; }
+
+	public Transform FireTransform
+	{
+		get { return _fireTransform; }
+	}
 	void Start()
 	{
+		//	_fireTransform.eulerAngles = new Vector3(0f, -_fireAngle, 0f);
 	}
 
-
-	void Update()
+	private void Update()
 	{
-		
+		_fireTransform.localEulerAngles = new Vector3(-_fireAngle, 0f, 0f);
 	}
 
 	public void RotateArcher(Vector3 _mousePos)
 	{
-		 _direction = _mousePos - transform.position;
-		 _directionXZ = new Vector3(_direction.x, 0, _direction.z);
-		
+		_direction = _mousePos - transform.position;
+		_directionXZ = new Vector3(_direction.x, 0, _direction.z);
+
 		transform.rotation = Quaternion.LookRotation(_directionXZ);
 	}
 
-	public void Shot()
+	public void CalcuateVelocity()
 	{
-		float x = _directionXZ.magnitude;
+		float x = _direction.magnitude;
 		float y = _direction.y;
 
 		float angleToRadians = _fireAngle * Mathf.PI / 180f;
 
 		float v2 = (Physics.gravity.y * x * x) / (2 * (y - Mathf.Tan(angleToRadians) * x) * Mathf.Pow(Mathf.Cos(angleToRadians), 2));
 		float v = Mathf.Sqrt(v2);
-		GameObject newProjectile = Instantiate(_projectile, _fireTransform.position, _fireTransform.rotation);
-		newProjectile.GetComponent<Rigidbody>().velocity = _fireTransform.forward * v;
+		_projectileVelocity = v;
+		ProjectileVelocity = _fireTransform.forward * v;
+	}
 
+	public void Shot()
+	{
+		GameObject newProjectile = Instantiate(_projectile, _fireTransform.position, _fireTransform.rotation);
+		newProjectile.GetComponent<Rigidbody>().velocity = _fireTransform.forward * _projectileVelocity;
 	}
 }
--- Assets/Archer_Project/Scripts/ProjectoryRenderer.cs
+++ Assets/Archer_Project/Scripts/ProjectoryRenderer.cs
@@ -7,12 +7,16 @@ public class ProjectoryRenderer : MonoBehaviour
 {
 	[SerializeField] private LayerMask _rayMask;
 	[SerializeField] private Archer _archer;
+	private LineRenderer _lineRenderer;
+	Vector3[] points = new Vector3[100];
+
 
 	public Vector3 MousePoint { get; private set; }
 
 	// Start is called before the first frame update
 	void Start()
 	{
+		_lineRenderer = GetComponentInChildren<LineRenderer>();
 	}
 
 	// Update is called once per frame
@@ -25,15 +29,27 @@ public class ProjectoryRenderer : MonoBehaviour
 
 			if (Physics.Raycast(mouseRay, out _hit, 100f, _rayMask))
 			{
-				Debug.Log("Hit Position" + _hit.point);
 				MousePoint = _hit.point;
 				_archer.RotateArcher(_hit.point);
+				_archer.CalcuateVelocity();
+
+				for (int i = 0; i < points.Length; i++)
+				{
+					float time = i * 0.1f;
+					points[i] = _archer.FireTransform.position + _archer.ProjectileVelocity * time + (Physics.gravity * Mathf.Pow(time, 2) / 2f);
+				}
+
+				_lineRenderer.enabled = true;
+				Debug.Log("points Length" + points.Length);
+				_lineRenderer.SetPositions(points);
 			}
 		}
+
 
 		if (Input.GetMouseButtonUp(0))
 		{
 			_archer.Shot();
+			_lineRenderer.enabled = false;
 		}
 	}
 }
